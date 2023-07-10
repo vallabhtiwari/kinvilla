5699ace0e2bfefdd09bf60eaaaebb5f45248e404
Vallabh <tiwarivallabh27@gmail.com>
2023-07-10 16:28:33 +0530

    created manager for custom user model

---
+++ user/manager.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        # email is mandatory
+        if not email:
+            raise ValueError("Please provide email")
+
+        # abc@email.com, ABC@email.com ,abc@EMAIL.com all are same
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        # set super user privileges
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        # create user
+        return self.create_user(email, password, **extra_fields)
