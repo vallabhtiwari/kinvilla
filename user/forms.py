7dc6f8c47949c2285a6f892c172f7725ee0196be
Vallabh <tiwarivallabh27@gmail.com>
2023-07-14 16:22:21 +0530

    added name fields for registration

--- user/forms.py
+++ user/forms.py
@@ -6,7 +6,9 @@ from .models import User
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    first_name = forms.TextInput()
+    last_name = forms.TextInput()
 
     class Meta:
         model = User
-        fields = ["email", "password1", "password2"]
+        fields = ["first_name", "last_name", "email", "password1", "password2"]
