63a45184f2d8f4c90eafefad5619ad2abf30fe1f
Vallabh <tiwarivallabh27@gmail.com>
2023-07-14 16:19:24 +0530

    urls for login and logout and some minor tweaks

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import CreateUserView
 
 app_name = "user"
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="register-user"),
+    path("register/", CreateUserView.as_view(), name="user-register"),
+    path(
+        "login/", LoginView.as_view(template_name="user/login.html"), name="user-login"
+    ),
+    path(
+        "logout/",
+        LogoutView.as_view(template_name="user/logout.html"),
+        name="user-logout",
+    ),
 ]
