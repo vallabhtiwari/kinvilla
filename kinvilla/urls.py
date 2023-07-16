46d6591717e788619e57a1c02981900b7ff8de6a
Vallabh <tiwarivallabh27@gmail.com>
2023-07-16 19:06:38 +0530

    views for room list and room detail

--- kinvilla/urls.py
+++ kinvilla/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("home.urls")),
     path("user/", include("user.urls")),
+    path("rooms/", include("room.urls")),
     path("admin/", admin.site.urls),
 ]
--- room/views.py
+++ room/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Room
 
 # Create your views here.
+class RoomListView(ListView):
+    model = Room
+
+    def get_queryset(self):
+        all_rooms = super().get_queryset()
+        current_floor_rooms = all_rooms.filter(floor=self.kwargs.get("floor")).order_by(
+            "occupied"
+        )
+
+        return current_floor_rooms
+
+
+class RoomDetailView(DetailView):
+    model = Room
+    pk_url_kwarg = "room_number"
