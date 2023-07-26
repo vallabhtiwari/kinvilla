1c60f35d734fb14a925abdc8d641681550c31fa8
Vallabh <tiwarivallabh27@gmail.com>
2023-07-26 14:38:07 +0530

    urls for views, verifiying user and bookings

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import CreateBookingView
+from .views import CreateBookingView, UpdateBookingView, UpdateVerificationiView
 
 app_name = "core"
 urlpatterns = [
     path("create-booking/", CreateBookingView.as_view(), name="create-booking"),
+    path(
+        "update-verification/<str:resident_id>/",
+        UpdateVerificationiView.as_view(),
+        name="update-verification",
+    ),
+    path(
+        "update-booking/<str:booking_id>/",
+        UpdateBookingView.as_view(),
+        name="update-booking",
+    ),
 ]
