21895d57e5c0a39caaab123abfe8f6c8021e91ef
Vallabh <tiwarivallabh27@gmail.com>
2023-07-27 12:59:05 +0530

    model for payments

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
+
+from user.models import Resident
+from room.models import Room
 
 # Create your models here.
+class Payment(models.Model):
+    payment_id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    transaction_id = models.CharField(max_length=100, null=True, blank=True)
+    payer = models.ForeignKey(
+        Resident, null=True, blank=True, on_delete=models.SET_NULL
+    )
+    amount = models.DecimalField(max_digits=7, decimal_places=2)
+    first_payment = models.BooleanField(default=False)
+    room = models.ForeignKey(Room, null=True, blank=True, on_delete=models.SET_NULL)
