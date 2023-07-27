45271fce18376b19a6930ced1e037577801e9019
Vallabh <tiwarivallabh27@gmail.com>
2023-07-27 18:35:49 +0530

    fields for razorpay payments

--- payment/models.py
+++ payment/models.py
@@ -6,13 +6,23 @@ from room.models import Room
 
 # Create your models here.
 class Payment(models.Model):
+    STATUS_CHOICE = [
+        ("0", "FAILURE"),
+        ("1", "SUCCESS"),
+        ("2", "PENDING"),
+    ]
     payment_id = models.UUIDField(
         primary_key=True, unique=True, editable=False, default=uuid.uuid4
     )
-    transaction_id = models.CharField(max_length=100, null=True, blank=True)
     payer = models.ForeignKey(
         Resident, null=True, blank=True, on_delete=models.SET_NULL
     )
     amount = models.DecimalField(max_digits=7, decimal_places=2)
     first_payment = models.BooleanField(default=False)
     room = models.ForeignKey(Room, null=True, blank=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICE, default="2")
+    date_of_payment = models.DateTimeField(null=True, blank=True)
+
+    razorpay_order_id = models.CharField(max_length=200, null=True, blank=True)
+    razorpay_payment_id = models.CharField(max_length=200, null=True, blank=True)
+    razorpay_signature = models.CharField(max_length=200, null=True, blank=True)
